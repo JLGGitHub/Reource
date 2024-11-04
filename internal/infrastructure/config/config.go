37ff1a6c88cdd1289bf9272c185545db0bf9ce7d
Pepe Gomez <josegomez@MacBook-Pro-de-Pepe.local>
2024-11-04 10:39:48 -0500

    is added config server

---
+++ internal/infrastructure/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DatabaseDSN string
+	ServerPort  string
+}
+
+func NewConfig() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, loading from environment variables")
+	}
+	return &Config{
+		DatabaseDSN: getEnv("DATABASE_DSN", "root:admin123@tcp(db:3306)/users?parseTime=true"),
+		ServerPort:  getEnv("SERVER_PORT", "8081"),
+	}
+}
+
+// Helper para obtener variables de entorno con un valor por defecto
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
